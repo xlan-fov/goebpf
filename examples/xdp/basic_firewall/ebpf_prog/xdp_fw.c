7986028f3a1e0f8628a1d97c6dce78e7d22113a9
xlan-fov <83161926+xlan-fov@users.noreply.github.com>
2024-10-16 11:04:21 +0800

    comment

--- examples/xdp/basic_firewall/ebpf_prog/xdp_fw.c
+++ examples/xdp/basic_firewall/ebpf_prog/xdp_fw.c
@@ -2,10 +2,13 @@
 #include <arpa/inet.h>
 #include "stdlib.h"
 
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#define __u128 __uint128_t
 #define MAX_RULES  128
 #define NANO_TO_SEC 1000000000
 #define default_pps 100
 #define default_bps 100000
+
 
 // 以太网头部
 struct ethhdr {
@@ -29,7 +32,20 @@ struct iphdr {
   __u32 saddr;  // 源IP地址
   __u32 daddr;  // 目的IP地址
 } __attribute__((packed));  
+// 紧凑排列
 
+//typedef __u8 __u128[16];
+
+struct ipv6hdr {
+  __u8 version : 4;  // 版本号，占4位
+  __u8 traffic_class : 8;  // 流量类别，占8位
+  __u32 flow_label : 20;  // 流标签，占20位
+  __u16 payload_len;  // 有效载荷长度
+  __u8 next_hdr;  // 下一个头部
+  __u8 hop_limit;  // 跳数限制
+  __u128 saddr;  // 源地址  
+  __u128 daddr;  // 目的地址
+} __attribute__((packed));
 
 struct ip_stats{
     __u64 pps;  //每秒数据包数
@@ -52,6 +68,22 @@ BPF_MAP_DEF(ip_blacklist) = {
     .max_entries = MAX_RULES,
 };
 BPF_MAP_ADD(ip_blacklist);
+
+BPF_MAP_DEF(ipv6_counter) = {
+    .map_type = BPF_MAP_TYPE_LRU_HASH,
+    .key_size = sizeof(__u128),
+    .value_size = sizeof(struct ip_stats),
+    .max_entries = MAX_RULES,
+};
+BPF_MAP_ADD(ipv6_counter);
+
+BPF_MAP_DEF(ipv6_blacklist) = {
+    .map_type = BPF_MAP_TYPE_LRU_HASH,
+    .key_size = sizeof(__u128),
+    .value_size = sizeof(__u32),
+    .max_entries = MAX_RULES,
+};
+BPF_MAP_ADD(ipv6_blacklist);
 
 BPF_MAP_DEF(config_pps) = {
     .map_type = BPF_MAP_TYPE_LRU_HASH,
@@ -85,6 +117,15 @@ BPF_MAP_DEF(unblock_time) = {
 };
 BPF_MAP_ADD(unblock_time);
 
+// eBPF map to store IP proto counters (tcp, udp, etc)
+BPF_MAP_DEF(protocols) = {
+    .map_type = BPF_MAP_TYPE_PERCPU_ARRAY,
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(__u64),
+    .max_entries = MAX_RULES,
+};
+BPF_MAP_ADD(protocols);
+
 // XDP program //
 SEC("xdp")
 int firewall(struct xdp_md *ctx) {
@@ -99,9 +140,9 @@ int firewall(struct xdp_md *ctx) {
     // Malformed Ethernet header
     return XDP_ABORTED;
   }
-  //检查以太网头部的协议字段是否为IPv4协议
-  if (ether->h_proto != 0x08U) {  // htons(ETH_P_IP) -> 0x08U
-    // 非IPv4数据包，直接放行
+  //检查以太网头部的协议字段是否为IPv4或IPv6协议
+  if (ether->h_proto != 0x08U &&ether->h_proto != 0xDD86U  ) {  // htons(ETH_P_IP) -> 0x08U, htons(ETH_P_IPV6) -> 0xDD86U
+    // 非IPv4或IPv6数据包，直接放行
     //bpf_printk("pass----------non ip packet\n");
     return XDP_PASS;
   }
@@ -115,7 +156,7 @@ int firewall(struct xdp_md *ctx) {
     return XDP_ABORTED;
   }
   __u32 saddr = ip->saddr;
-  saddr = htonl(saddr); 
+  saddr = htonl(saddr);
   //bpf_printk("IPv4:%d--------%x\n",saddr,saddr);
   blocked=bpf_map_lookup_elem(&ip_blacklist, &saddr);
   ip_stats_pointer=bpf_map_lookup_elem(&ip_counter, &saddr);
@@ -131,19 +172,20 @@ int firewall(struct xdp_md *ctx) {
       bpf_printk("block_time or unblock_time is NULL\n");
       return XDP_DROP;
     }
-    if (*block_time_value > 0) {
-      bpf_printk("Blocked forever:%d---------%x\n",saddr,saddr);
-      return XDP_DROP;
-    }
     if (ip_stats_pointer) {
       if (now - ip_stats_pointer->next_update >= (*unblock_time_value)*NANO_TO_SEC) {
         bpf_map_delete_elem(&ip_blacklist, &saddr);
         ip_stats_pointer->pps = 1;
         ip_stats_pointer->bps = pkt_len ;
         ip_stats_pointer->next_update = now + NANO_TO_SEC;
-        bpf_map_update_elem(&ip_counter, &saddr, ip_stats_pointer, BPF_ANY);
         return XDP_PASS;
       }
+    } else {
+      struct ip_stats new_ip_stats={0};
+      new_ip_stats.pps = 1;
+      new_ip_stats.bps = pkt_len;
+      new_ip_stats.next_update = now + NANO_TO_SEC;
+      bpf_map_update_elem(&ip_counter, &saddr, &new_ip_stats, BPF_ANY);
     }
     bpf_printk("Blocked:%d---------%x\n",saddr,saddr);
     return XDP_DROP;
